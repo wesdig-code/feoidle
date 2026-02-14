652f31b47c6849ccae10b166cf0a69787d8de7a5
Feoidle Agent <feoidle@local>
2026-02-14 22:22:32 +0100

    :art: make game canvas fullscreen and style body

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 import Phaser from 'phaser'
+import TitleScene from './scenes/TitleScene.js'
 import MainScene from './scenes/MainScene.js'
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  backgroundColor: '#222',
   parent: 'game',
-  scene: [MainScene]
+  backgroundColor: '#222',
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: window.innerWidth,
+    height: window.innerHeight
+  },
+  scene: [TitleScene, MainScene]
 }
 
-window.addEventListener('load', () => {
-  new Phaser.Game(config)
-})
+new Phaser.Game(config)
