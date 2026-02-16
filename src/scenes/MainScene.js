e1997c1f465bf6c685d30e1ada5ad46d15d0d7dc
Feoidle Agent <feoidle@local>
2026-02-16 22:14:09 +0100

    :sparkles: add inventory toggle button

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser'
+import OrnateButton from '../ui/OrnateButton.js'
 
 export default class MainScene extends Phaser.Scene {
   constructor() {
@@ -22,25 +23,32 @@ export default class MainScene extends Phaser.Scene {
 
     this.cameras.main.setBackgroundColor(0x131313)
 
-    const g = this.add.graphics()
+    const inventoryContainer = this.add.container(0, 0)
+    const invG = this.add.graphics()
+    inventoryContainer.add(invG)
     const gold = 0xd6b074
     const panel = 0x2a1b16
     const panelLight = 0x3a261f
 
+    const addInv = (obj) => {
+      inventoryContainer.add(obj)
+      return obj
+    }
+
     const drawPanel = (x, y, width, height) => {
-      g.fillStyle(panel, 1)
-      g.fillRect(x, y, width, height)
-      g.lineStyle(3, gold, 1)
-      g.strokeRect(x + 1, y + 1, width - 2, height - 2)
-      g.lineStyle(1, 0x0c0c0c, 1)
-      g.strokeRect(x + 6, y + 6, width - 12, height - 12)
+      invG.fillStyle(panel, 1)
+      invG.fillRect(x, y, width, height)
+      invG.lineStyle(3, gold, 1)
+      invG.strokeRect(x + 1, y + 1, width - 2, height - 2)
+      invG.lineStyle(1, 0x0c0c0c, 1)
+      invG.strokeRect(x + 6, y + 6, width - 12, height - 12)
     }
 
     const drawSlot = (x, y, size) => {
-      g.fillStyle(panelLight, 1)
-      g.fillRect(x, y, size, size)
-      g.lineStyle(2, gold, 1)
-      g.strokeRect(x + 1, y + 1, size - 2, size - 2)
+      invG.fillStyle(panelLight, 1)
+      invG.fillRect(x, y, size, size)
+      invG.lineStyle(2, gold, 1)
+      invG.strokeRect(x + 1, y + 1, size - 2, size - 2)
     }
 
     // Inventory column (25% width, left side)
@@ -51,13 +59,13 @@ export default class MainScene extends Phaser.Scene {
 
     // Top bar (left column)
     drawPanel(colX, margin, colW, topBarH)
-    this.add.text(colX + colW / 2, margin + topBarH / 2, 'INVENTORY', {
+    addInv(this.add.text(colX + colW / 2, margin + topBarH / 2, 'INVENTORY', {
       fontFamily: 'Georgia, serif',
       fontSize: '22px',
       color: '#f4e1b5',
       stroke: '#2b120a',
       strokeThickness: 4
-    }).setOrigin(0.5)
+    }).setOrigin(0.5))
 
     // Left stats panel
     const leftX = colX
@@ -75,12 +83,12 @@ export default class MainScene extends Phaser.Scene {
       `Armor: ${playerStats.armor}`
     ]
 
-    this.add.text(leftX + 14, leftY + 14, statText.join('\n'), {
+    addInv(this.add.text(leftX + 14, leftY + 14, statText.join('\n'), {
       fontFamily: 'Arial, sans-serif',
       fontSize: '13px',
       color: '#e6d9c2',
       lineSpacing: 8
-    })
+    }))
 
     // Bottom inventory grid
     const gridX = colX
@@ -99,6 +107,16 @@ export default class MainScene extends Phaser.Scene {
         drawSlot(startX + c * 48, startY + r * 48, 44)
       }
     }
+
+    // Inventory toggle button (bottom center)
+    let inventoryVisible = true
+    const toggleBtn = new OrnateButton(this, 'Inventaire', w / 2, h - 40, 220, 52, {
+      fontSize: '20px'
+    })
+    toggleBtn.on('pointerdown', () => {
+      inventoryVisible = !inventoryVisible
+      inventoryContainer.setVisible(inventoryVisible)
+    })
 
   }
 }
